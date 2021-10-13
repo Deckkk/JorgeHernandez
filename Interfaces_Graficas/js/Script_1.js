fbe6a080204e6fd40cee6717e593875df3bc11dc
Jorge Hernández Flores <76122370+jorgeherflo@users.noreply.github.com>
2021-10-13 16:43:52 +0200

    Add files via upload

---
+++ Interfaces_Graficas/js/Script_1.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var y = document.getElementById('TemasDesplegable');
+    if ( y.style.top === '-1000px' ) {
+      y.style.top = '270px';
+    } else { 
+        y.style.top = '-1000px';
+    }
+}
---
+++ js/Script_1.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  var x = document.getElementById('header1');
+  var y = document.getElementById('slider');
+  var j = document.getElementById('myBtn');
+  var h = document.getElementById('Eslogan');
+  if ( (x.style.top === '-150px') && (y.style.top === '-700px') ) {
+    x.style.top = '0px';
+    y.style.top = '0px';
+    j.style.top = '85%';
+    h.style.visibility = 'hidden';
+  } else {
+    x.style.top = '-150px';
+    y.style.top = '-700px';
+    j.style.top = '60%';
+    h.style.visibility = 'visible';
+  }
+}
