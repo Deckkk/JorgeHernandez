5d86b77d5250fa3e94abd3cc1a2152de20db70e2
Jorge Hernández Flores <76122370+jorgeherflo@users.noreply.github.com>
2021-10-14 14:30:09 +0200

    14/10/2021
    Desarrollo Interfaces ( Index), Tema 1 (completo).

---
+++ Interfaces_Graficas/temas/tema_1/js/Script_1.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var y = document.getElementById('TemasDesplegable');
+      if ( y.style.left === '-100%' ) {
+        y.style.left = '0%';
+      } else { 
+          y.style.left = '-100%';
+      }
+  }
+  
