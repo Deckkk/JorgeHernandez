2b9e083462b621ec05cc5de6743213a9c604c400
Jorge Hernández Flores <76122370+jorgeherflo@users.noreply.github.com>
2021-10-25 11:59:14 +0200

    25/10/2021 casa index.

--- js/Script_1.js
+++ js/Script_1.js
@@ -3,14 +3,14 @@ function myFunction() {
   var y = document.getElementById('slider');
   var j = document.getElementById('myBtn');
   var h = document.getElementById('Eslogan');
-  if ( (x.style.top === '-150px') && (y.style.top === '-700px') ) {
+  if ( (x.style.top === '-150px') && (y.style.top === '-2000px') ) {
     x.style.top = '0px';
     y.style.top = '0px';
     j.style.top = '85%';
     h.style.visibility = 'hidden';
   } else {
     x.style.top = '-150px';
-    y.style.top = '-700px';
+    y.style.top = '-2000px';
     j.style.top = '60%';
     h.style.visibility = 'visible';
   }
