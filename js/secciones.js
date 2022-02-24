0982e469b445fa897a0681c75a012bf1f210bbc3
Jorge 2 <76122370+jorgeherflo@users.noreply.github.com>
2022-02-24 18:50:29 +0100

    commit 24 - 2 - 2022

--- js/secciones.js
+++ js/secciones.js
@@ -16,3 +16,15 @@ function displaySec(posicion) {
  }
 
 }
+
+// function displaySec3(posicion) {
+
+//     for ( let el of secciones ) {
+//         if ( el.id == secciones[posicion].id ) {
+//             el.style.display = 'block';
+//         } else {
+//             el.style.display = 'none';
+//         }
+//     }
+
+// }
